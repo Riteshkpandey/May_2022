11811ab7c7e7a3766be0cdb2ca13dd566cddfcbc
RITESH KUMAR PANDEY <39904997+Riteshkpandey@users.noreply.github.com>
2022-06-08 20:21:44 +0530

    Time: 86 ms (40.33%), Space: 41.7 MB (96.26%) - LeetHub

--- 48-rotate-image/48-rotate-image.js
+++ 48-rotate-image/48-rotate-image.js
@@ -4,25 +4,33 @@
  */
 var rotate = function(matrix) {
     
+    // transposing the matrix
+    
     for(let i=0;i<matrix.length;i++)
         {
             for(let j=i;j<matrix[0].length;j++)
                 {
+                    if(i!=j)
+                        {
                             let temp=matrix[i][j];
-                            matrix[i][j]=matrix[j][i];
+                            matrix[i][j]=matrix[j][i]
                             matrix[j][i]=temp;
-                        
+                        }
+                    
                 }
+            
         }
-    console.log(matrix);
     
+    // swapping the ith col with n-1-i col
+
     for(let i=0;i<matrix.length;i++)
         {
             for(let j=0;j<matrix[0].length/2;j++)
                 {
+                    let lastCol=matrix[0].length-1-j;
                     let temp=matrix[i][j];
-                    matrix[i][j]=matrix[i][matrix[0].length-1-j];
-                    matrix[i][matrix[0].length-1-j]=temp;
+                    matrix[i][j]=matrix[i][lastCol];
+                    matrix[i][lastCol]=temp;
                 }
         }
     return matrix;
